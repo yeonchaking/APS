2698a8101d9d21492ea45ad8595fc1d0187a0650
greenteaking <99801068+yeonchaking@users.noreply.github.com>
2024-01-04 14:14:00 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 244 ms, Memory: 18448 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+
+        for(int i=-999;i<=999;i++){
+            for(int j=-999;j<=999;j++){
+                if(a*i+b*j==c&&d*i+e*j==f){
+                    System.out.println(i+" "+j);
+                    return;
+                }
+            }
+        }
+
+    }
+
+
+
+}
+
