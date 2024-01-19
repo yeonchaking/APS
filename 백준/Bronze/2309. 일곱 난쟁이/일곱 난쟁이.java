a4d7fae1af562bdfabe34cad2f52f58bacfc6464
greenteaking <99801068+yeonchaking@users.noreply.github.com>
2024-01-19 12:42:33 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,36 +1,47 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Writer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int[] arr = new int[9];
-        int sum = 0;
+
+    static boolean[] light ;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[]arr = new int[9];
+        int sum =0;
         for(int i=0;i<9;i++){
-            arr[i]=sc.nextInt();
-            sum+= arr[i];
+            arr[i]=Integer.parseInt(br.readLine());
+            sum+=arr[i];
         }
 
         Arrays.sort(arr);
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(i==j)continue;
-                if(sum-arr[i]-arr[j]==100){
-                    for(int k=0;k<9;k++){
-                        if(k==i||k==j)continue;
-                        System.out.println(arr[k]);
-                    }
-                    return;
+
+        int s =0;
+        int e = 8;
+
+        int spy = sum-100;
+
+        while (s<e){
+            int p = arr[s]+arr[e];
+            if(p>spy){
+                e--;
+            } else if (p<spy) {
+                s++;
+            }else {
+                for(int i=0;i<9;i++){
+                    if(i==s||i==e)continue;
+                    System.out.println(arr[i]);
                 }
+                return;
             }
         }
 
 
-
-
-
     }
+
+
 
 }
