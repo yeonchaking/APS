a74ea826e7076575d79b90e610e683db47699c97
greenteaking <99801068+yeonchaking@users.noreply.github.com>
2024-02-14 15:35:46 +0900

    [Gold III] Title: Sleeping in Class, Time: 284 ms, Memory: 23680 KB -BaekjoonHub

--- 백준/Gold/24620. Sleeping in Class/Sleeping in Class.java
+++ 백준/Gold/24620. Sleeping in Class/Sleeping in Class.java
@@ -1,77 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.*;
 
 public class Main {
 
-    static boolean[] isPrime;
-
+    static int n;
+    static int [] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-
-        for (int tc = 1; tc <= T; tc++) {
-            int N = Integer.parseInt(br.readLine());
+        TestCase : for(int tc=1;tc<=T;tc++){
+            n = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
-            long[] arr = new long[N];
-            long sum = 0;
-
-
-            for (int i = 0; i < N; i++) {
-                arr[i] = Long.parseLong(st.nextToken());
-                sum+=arr[i];
-            }
-            if(check(N,arr[0],arr)) {
-                sb.append(0+"\n");
-                continue;
+            arr = new int[n];
+            int prefixSum=0;
+            for(int i=0;i<n;i++){
+                arr[i]=Integer.parseInt(st.nextToken());
+                prefixSum+=arr[i];
             }
 
-            ArrayList<Long> list = new ArrayList<>();
-            list.add((long)1);
-            list.add(sum);
-            for (long i = 2; i <= Math.sqrt(sum); i++) {
-                if (sum % i == 0) {
-                    list.add(i);
-                    list.add(sum / i);
+            if(prefixSum==0){
+                sb.append(0).append("\n");
+                continue TestCase;
+            }
+            TreeSet<Integer> set = new TreeSet<>();
+            set.add(1);
+            set.add(prefixSum);
+            for(int i=2;i<=Math.sqrt(prefixSum);i++){
+                if(prefixSum%i==0){
+                    set.add(i);
+                    set.add(prefixSum/i);
                 }
             }
 
-            long min = Long.MAX_VALUE;
-            for(long i : list){
-                if(check(sum/i,i,arr)){
-                   min = Math.min(N-sum/i,min);
+            for(int i : set){
+                if(i==0)continue ;
+                if(check(i,prefixSum)){
+                    sb.append((n - prefixSum / i)).append("\n");
+                    continue TestCase;
                 }
+
             }
-            sb.append(min+"\n");
+
 
         }
         System.out.println(sb.toString());
+
     }
 
+    static boolean check(int x,int prefixSum){
+        int sum = 0;
+        int cnt =0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
 
-    static boolean check(long len, long num, long[] arr){
-        int idx =0;
-        long sum =0;
-        long cnt =0;
-        while (idx<arr.length){
-            sum+=arr[idx];
-            if(sum>num){
-                return false;
-            } else if (sum<num) {
-                idx++;
-                continue;
-            }else {
-                idx++;
+            if(sum>x)return false;
+            else if(sum==x){
                 sum=0;
                 cnt++;
             }
         }
-        return cnt == len;
-
-
+        if(sum==0&&x*cnt==prefixSum)return true;
+        else return false;
     }
 
 
