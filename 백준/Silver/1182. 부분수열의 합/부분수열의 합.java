ee03c951ec65097ab5d30dc2fe85098bec779067
greenteaking <99801068+yeonchaking@users.noreply.github.com>
2024-02-22 06:09:16 +0900

    [Silver II] Title: 부분수열의 합, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,62 +1,42 @@
 import java.io.BufferedReader;
         import java.io.IOException;
         import java.io.InputStreamReader;
-        import java.security.spec.RSAOtherPrimeInfo;
-        import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n,s;
+
+    static int n,m,cnt;
     static int[] arr;
-    static boolean[] visited;
-    static int cnt;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n= Integer.parseInt(st.nextToken());
-        s= Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
+
+        n =  Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        cnt = 0;
         arr = new int[n];
-        visited = new boolean[n];
-        cnt=0;
+        st = new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
             arr[i]=Integer.parseInt(st.nextToken());
         }
-
-
-            recur(0);
-
+        recur(0,0,false);
         System.out.println(cnt);
-
-
     }
 
-    static void recur(int cur){
-
+    static void recur(int cur,int sum,boolean check){
         if(cur==n){
-            int sum = 0;
-            boolean c=false;
-            for(int i=0;i<n;i++){
-                if(visited[i]){
-                    sum+=arr[i];
-                    c=true;
-                }
-            }
-
-            if(sum==s&&c){
-//                System.out.println(Arrays.toString(visited));
+            if(sum==m&&check){
                 cnt++;
             }
             return;
         }
 
-
-
-
-            recur(cur+1);
-            visited[cur]=true;
-            recur(cur+1);
-            visited[cur]=false;
-
+        recur(cur+1,sum+arr[cur],true);
+        recur(cur+1,sum,check);
     }
+
+
+
 }
