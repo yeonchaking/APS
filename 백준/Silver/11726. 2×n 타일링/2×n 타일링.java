c12aff2aa6fc12398d78a38fbd98303042ca4e4f
greenteaking <99801068+yeonchaking@users.noreply.github.com>
2024-03-16 23:09:13 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int [] arr = new int[n];
+
+        arr[0]=1;
+        if(n>1)arr[1]=2;
+
+        for(int i=2;i<n;i++){
+            arr[i]=(arr[i-1]+arr[i-2])%10007;
+        }
+        System.out.println(arr[n-1]);
+
+
+    }
+}
