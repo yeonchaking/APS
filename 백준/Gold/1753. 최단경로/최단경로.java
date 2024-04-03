c88fc7a52a47b30aa889d47b9394db3d75a5110f
greenteaking <99801068+yeonchaking@users.noreply.github.com>
2024-04-03 15:37:33 +0900

    [Gold IV] Title: 최단경로, Time: 724 ms, Memory: 107980 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,86 +1,66 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.*;
 
 public class Main {
-	static class Node implements Comparable<Node> {
-		int v, w;
+    static int INF = Integer.MAX_VALUE;
+    static class Node implements Comparable<Node>{
+        int v,w;
+        public Node(int v, int w){
+            this.v=v;
+            this.w=w;
+        }
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.w,o.w);
+        }
+    }
 
-		public Node(int v, int w) {
-			this.v = v;
-			this.w = w;
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+       PriorityQueue<Node> pq = new PriorityQueue<>();
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
 
-		@Override
-		public int compareTo(Node o) {
-			return Integer.compare(this.w, o.w);
-		}
-	}
+        int k = Integer.parseInt(br.readLine());
+        ArrayList<Node>[] adjList = new ArrayList[v+1];
 
-	static final int INF = Integer.MAX_VALUE;
-	static int V, E;
-	static List<Node>[] adjList;
-	static int[] dist;
+        for(int i=0;i<=v;i++)adjList[i]=new ArrayList<>();
+        for(int i=0;i<e;i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+            adjList[x].add(new Node(y,z));
+        }
 
-		V = sc.nextInt();
-		E = sc.nextInt();
+       int[] dist = new int[v+1];
+        Arrays.fill(dist,INF);
 
-		int K = sc.nextInt();
-		adjList = new ArrayList[V + 1];
-		for (int i = 1; i <= V; i++) {
-			adjList[i] = new ArrayList<>();
-		}
+        dist[k]=0;
+        pq.add(new Node(k,0));
 
-		dist = new int[V + 1];
-		Arrays.fill(dist, INF);
+        while (!pq.isEmpty()){
+            Node now = pq.poll();
+            if(dist[now.v]<now.w)continue;
 
-		// 입력 받기
-		for (int i = 0; i < E; i++) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
-			int W = sc.nextInt();
-
-			// 유향 그래프였다
-			adjList[A].add(new Node(B, W)); // 인접리스트 노드 추가
-		}
-
-		dijkstra(K);
-
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = 1; i <= V; i++) {
-			if (dist[i] == INF) {
-				sb.append("INF\n");
-			} else {
-				sb.append(dist[i] + "\n");
-			}
-		}
-		System.out.println(sb.toString());
-	}
-
-	private static void dijkstra(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		dist[start] = 0; // 시작노드까지의 거리는 0
-		pq.offer(new Node(start, 0));
-
-		while (!pq.isEmpty()) {
-			Node now = pq.poll();
-
-			if (dist[now.v] < now.w)
-				continue; // 이미 처리된 노드면 skip
-
-			for (Node next : adjList[now.v]) {
-				int newDist = dist[now.v] + next.w;
-				if (dist[next.v] > newDist) { // 거리가 갱신되는 경우
-					dist[next.v] = newDist;
-					pq.offer(new Node(next.v, newDist));
-				}
-			}
-		}
-	}
+            for(Node n : adjList[now.v]){
+                int cur = dist[now.v]+n.w;
+                if(dist[n.v]>cur){
+                    dist[n.v]=cur;
+                    pq.add(new Node(n.v,cur));
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<=v;i++){
+            if(dist[i]==INF)sb.append("INF").append("\n");
+            else sb.append(dist[i]).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
 }
