3e92f861b4c2fad8d42be85501073eaf3b5db4e6
greenteaking <99801068+yeonchaking@users.noreply.github.com>
2024-04-04 23:32:37 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 488 ms, Memory: 91380 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.java
@@ -1,64 +1,65 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N  = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[]arr = new int[N];
-        st =  new StringTokenizer(br.readLine());
-        for(int i=0;i<N;i++){
-            arr[i] =Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int s1 = 0;
-        int s2 = 0;
-        int cnt =0;
-        int len =0;
-        int max = 0;
-        if(arr[s2]%2!=0)cnt++;
-        else len++;
-        while(s2<N){
-            if(max<len){
-                max = len;
-            }
-            if(cnt<=K){
-                s2++;
-                if(s2<N&&arr[s2]%2==0){
-                    len++;
-                }else if(s2==N) {
-                    if(max<len){
-                        max = len;
-                    }
+        // 입력 완료
+
+        int answer = 0;
+        int left = 0, right = 0;
+        int odd = 0, even = 0;//맨 앞꺼 홀수 체크
+
+        if(arr[right]%2==0)even++;
+        else odd++;
+        while(right<N) {
+            //System.out.println("left " + left+" right  "+ right);
+            answer = Math.max(answer, even);
+
+            if(odd<=K) {
+                right++;
+                if(right == N) {
+                    answer = Math.max(answer, even);
                     break;
-                }else {
-                     cnt++;
                 }
 
-//                System.out.println("s2증가"+s1+" "+s2+" 홀수는 "+cnt+"개 있고 길이는"+len);
-            }else {
-                if(arr[s1]%2==0){
-                    len--;
-                }else {
-                    cnt--;
+                if(arr[right]%2==1) {
+                    odd++;
                 }
-                s1++;
-//                System.out.println("s1증가"+s1+" "+s2+" 홀수는 "+cnt+"개 있고 길이는"+len);
+                else {
+                    even++;
+                }
+
             }
+
+            else {
+                if(arr[left]%2==1) { //홀수면
+                    odd--;
+                }
+                else {
+                    even--;
+                }
+                left++;
+            }
+
+            //System.out.println("홀 " + odd + " 짝 " + even);
+
         }
 
-
-        System.out.println(max);
-
-
+        System.out.println(answer);
 
     }
-
-
-
-
 
 }
